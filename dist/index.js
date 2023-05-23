cf5fce1035b421229a96d38ad111031c45862554
Luca Ban <mesqueeb@gmail.com>
2023-05-23 11:20:52 +0900

    fix: exports

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+function mapGetOrSet(map, key, initialValue) {
+  let val = map.get(key);
+  if (val === void 0) {
+    val = initialValue();
+    map.set(key, val);
+  }
+  return val;
+}
+function objGetOrSet(obj, key, initialValue) {
+  let val = obj[key];
+  if (val === void 0) {
+    val = initialValue();
+    obj[key] = val;
+  }
+  return val;
+}
+
+export { mapGetOrSet, objGetOrSet };
